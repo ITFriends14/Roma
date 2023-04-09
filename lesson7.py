fb7affdc770beb52a681cd7b2d4d3eb867adc3b7
ITFriends14 <128975094+ITFriends14@users.noreply.github.com>
2023-04-09 15:57:49 +0300

    Add files via upload

---
+++ lesson7.py
@@ -0,0 +1,45 @@
+import turtle
+
+# for
+# while
+
+# for i in range(10):
+#     print(i)
+
+# for i in ["Roma", "eat", "is", 'something']:
+#     print(i)
+#     print(1)
+
+# for i in range(56):
+#     print(i)
+
+# for i in range(10):
+#     txt = input("How are you?")
+#   print(txt)
+
+# run = 100
+# while run:
+#    print(100-7)
+#    run -= 1
+
+turtle.color('purple')
+turtle.bgcolor('black')
+
+for i in range(4):
+    turtle.left(90)
+    turtle.forward(100)
+
+turtle.clear()
+
+for i in range(25):
+    turtle.right(15)
+    turtle.forward(10)
+
+
+
+
+
+
+
+
+
